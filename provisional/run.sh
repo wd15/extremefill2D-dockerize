f072c26f047c43cf138ae6d364c6297e414c59ea
Daniel Wheeler <daniel.wheeler2@gmail.com>
2017-04-17 15:35:35 -0400

    Update the provisonal run script

--- provisional/run.sh
+++ provisional/run.sh
@@ -1,7 +1,6 @@
 COUNTER=0
 for BULKSUPPRESSOR in "0.006" "0.012"
 do
-    # for APPLIEDPOTENTIAL in "-0.16" "-0.18" "-0.20" "-0.22"
     for APPLIEDPOTENTIAL in "-0.16" "-0.18" "-0.20" "-0.22" "-0.24"
     do
         smt run -t provisional2 provisional/params.json totalSteps=2000 appliedPotential="${APPLIEDPOTENTIAL}" bulkSuppressor="${BULKSUPPRESSOR}" > out.${COUNTER} 2>&1 &
